84809122c0584cffb6dbf47b62265aa777004047
poplink <2219535001@qq.com>
2022-12-12 20:42:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+```shell
+cd client
+# 启动客户端
+npm run start
+```
---
+++ client/routes/users.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const useName = require("../utils/useName");
+const router = express.Router();
+
+router.post("/login", function (req, res, next) {
+  const { username, password } = req.body;
+  res.cookie("userId", String(username), {
+    expires: new Date(Date.now() + 1000 * 60 * 60),
+    httpOnly: true,
+    signed: true,
+  });
+  res.send("success");
+});
+
+router.get("/name", function (req, res, next) {
+  const [name, setName] = useName();
+
+  res.send(name);
+});
+
+router.post("/name", function (req, res, next) {
+  const [name, setName] = useName();
+  const { name: updateName } = req.body;
+
+  res.send(setName(updateName));
+});
+
+router.post("/v2/login", function (req, res, next) {
+  const { username, password } = req.body;
+  res.cookie("userId", String(username), {
+    expires: new Date(Date.now() + 1000 * 60 * 60),
+    httpOnly: true,
+    signed: true,
+  });
+  res.send("success");
+});
+
+router.get("/name", function (req, res, next) {
+  const [name, setName] = useName();
+
+  res.send(name);
+});
+
+router.post("/name", function (req, res, next) {
+  const [name, setName] = useName();
+  const { name: updateName } = req.body;
+
+  res.send(setName(updateName));
+});
+
+module.exports = router;
